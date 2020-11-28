9159d186e494b3a65bd8e41717adf22e10483dc7
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-11-28 11:44:56 +0900

    アソシエーションのコロンの忘れ

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_many :room_users
   # 中間テーブルを通して繋がっているものにはthrough:というkeyをつける。
-  has_many :users, through: room_users
+  has_many :users, through: :room_users
 end
