3998086c37b66c34e59270871939f0c4f29a83d4
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-11-29 18:51:32 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,11 @@ class Message < ApplicationRecord
   # 各レコードとファイルを1対1の関係で紐づけるメソッドです。has_one_attachedメソッドを記述したモデルの各レコードは、
   # それぞれ1つのファイルを添付できます。has_one_attached :ファイル名
   has_one_attached :image
-  validates :content, presence: true
+  # was_attached?メソッドがfalseならばバリデーションによる検証を行う」という条件。
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    # 画像があればtrue、なければfalseを返す仕組み。これにより、画像が存在しなければテキストが必要となり、画像があればテキストは不要になる。
+    self.image.attached?
+  end
 end
