069e321c465e0417987a7fff66ec007ed2d7e406
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-11-29 18:40:04 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  # 各レコードとファイルを1対1の関係で紐づけるメソッドです。has_one_attachedメソッドを記述したモデルの各レコードは、
+  # それぞれ1つのファイルを添付できます。has_one_attached :ファイル名
   has_one_attached :image
   validates :content, presence: true
 end
